9f19b9dc39c433ac5b04577c6c547b01fc8caf45
Rohit Shaw <carpediem212045@gmail.com>
2022-11-02 20:51:46 +0530

    Implemeted marvel comic packages

--- Gear-Up/gUp.js
+++ Gear-Up/gUp.js
@@ -1,6 +1,6 @@
 const superheroes = require('superheroes'); // Loading superheroes module
 superheroes.all; //=> ['3-D
-console.log(superheroes.random()); //=> 'Spider-Ham'
+console.log("One of mine Superhero is: " + superheroes.random()); //=> 'Spider-Ham'
 
 //______________________________________________________________________________________________
 
@@ -8,4 +8,14 @@ var names = require('marvel-comics-characters'); // Loading marvel-comics-charac
 var allNames = names.all;
 var randomName = names.random(); // random name
 var threeRandomNames = names.random(3); // returns an array of 3 random names
-console.log(threeRandomNames);  // Prints 3 random names in list format
+// console.log("Here's the list of all the Marvel Comics character: \n"+ allNames);  
+console.log("\nRandom pick-up amongst the ones: "+ randomName);  
+console.log("\nThe Random Trio: "+ threeRandomNames);  // Prints 3 random names in list format
+
+
+//______________________________________________________________________________________________
+
+const supervillians = require('supervillains'); // Loading supervillians module
+supervillians.all;
+// console.log(supervillians.random()); //=> 'Agent Smith'
+console.log("\nThe Super Villian I don't wanna spit upon: " + supervillians.random()); //=> 'Agent Smith'
